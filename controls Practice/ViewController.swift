9d547f4a1421816df312e50c20647e5d832365e3
Stas Borovcov <sborovcov@gmail.com>
2020-12-21 22:42:02 +0700

    Working UI elements except switch

--- controls Practice/ViewController.swift
+++ controls Practice/ViewController.swift
@@ -14,21 +14,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
+    var number: UInt8 = 128{ //U - беззнаковое число
+        didSet{
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    /// Updates all outlets to number
+    func updateUI(){
+        button.setTitle("\(number)", for: []) // [] - для всех состояний
+        
+        // TODO: set switches to number
+        slider.value = Float(number)
+        textField.text = "\(number)"
+    }
+    
     @IBAction func buttonPressed() {
+        //print(#line, #function)
+        number = UInt8((Int(number) + 1) % 256) // берем по модулю 256б что бы не выйти за границы типа Int8
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
+        //print(#line, #function, sender.tag)
     }
     
     @IBAction func sliderMoved() {
+        //print(#line, #function)
+        number = UInt8(slider.value)
     }
     
     @IBAction func textFieldEdited() {
+        //print(#line, #function)
+        number = UInt8(textField.text ?? "") ?? 128 // text тут опциональное  ?? показывает,что если оно nil, то вернуть пустую строку
     }
 }
 
